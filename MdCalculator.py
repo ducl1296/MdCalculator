00cdfce10f6471fffa63ee6fb7306a31a9a78999
N3m0xxx <ducl1296@gmail.com>
2024-12-22 17:25:40 -0600

    change directory for remove navbar

--- MdCalculator.py
+++ MdCalculator.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🩺 MdCalculator")
 
-st.page_link("pages/BalanceHidrico.py", label="Balance Hidrico", icon="🚰")
+st.page_link("dir/BalanceHidrico.py", label="Balance Hidrico", icon="🚰")
 
